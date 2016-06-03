9fd8b58484f75a1126080cba951897d5fa6cab20
delasalle-sio-jim <jean.michel.cartron@gmail.com>
2016-06-03 09:30:38 +0200

    sw

--- services/ConsulterReservations.php
+++ services/ConsulterReservations.php
@@ -1,7 +1,7 @@
 <?php
 // Service web du projet Réservations M2L
 // Ecrit le 31/3/2016 par Jim
-// Modifié le 2/6/2016 par Jim
+// Modifié le 3/6/2016 par Jim
 
 // Ce service web permet à un utilisateur de consulter ses réservations à venir
 // et fournit un flux XML contenant un compte-rendu d'exécution
@@ -13,41 +13,30 @@
 // Les paramètres peuvent être passés par la méthode POST (à privilégier en exploitation pour la confidentialité des données) :
 //     http://<hébergeur>/ConsulterReservations.php
 
-// déclaration des variables globales pour pouvoir les utiliser aussi dans les fonctions
-global $doc;		// le document XML à générer
-global $nom, $lesReservations, $nbReponses;
-
 // inclusion de la classe Outils
 include_once ('../modele/Outils.class.php');
 // inclusion des paramètres de l'application
 include_once ('../modele/parametres.localhost.php');
-
-// crée une instance de DOMdocument 
-$doc = new DOMDocument();
-
-// specifie la version et le type d'encodage
-$doc->version = '1.0';
-$doc->encoding = 'ISO-8859-1';
-  
-// crée un commentaire et l'encode en ISO
-$elt_commentaire = $doc->createComment('Service web ConsulterReservations - BTS SIO - Lycée De La Salle - Rennes');
-// place ce commentaire à la racine du document XML
-$doc->appendChild($elt_commentaire);
 	
 // Récupération des données transmises
 // la fonction $_GET récupère une donnée passée en paramètre dans l'URL par la méthode GET
 if ( empty ($_GET ["nom"]) == true)  $nom = "";  else   $nom = $_GET ["nom"];
 if ( empty ($_GET ["mdp"]) == true)  $mdp = "";  else   $mdp = $_GET ["mdp"];
+
 // si l'URL ne contient pas les données, on regarde si elles ont été envoyées par la méthode POST
 // la fonction $_POST récupère une donnée envoyées par la méthode POST
 if ( $nom == "" && $mdp == "" )
 {	if ( empty ($_POST ["nom"]) == true)  $nom = "";  else   $nom = $_POST ["nom"];
 	if ( empty ($_POST ["mdp"]) == true)  $mdp = "";  else   $mdp = $_POST ["mdp"];
 }
-  
+
+// initialisation du nombre de réservations
+$nbReponses = 0;
+$lesReservations = array();
+
 // Contrôle de la présence des paramètres
 if ( $nom == "" || $mdp == "" )
-{	TraitementAnormal ("Erreur : données incomplètes.");
+{	$msg = "Erreur : données incomplètes.";
 }
 else
 {	// connexion du serveur web à la base MySQL ("include_once" peut être remplacé par "require_once")
@@ -55,7 +44,7 @@ else
 	$dao = new DAO();
 	
 	if ( $dao->getNiveauUtilisateur($nom, $mdp) == "inconnu" )
-		TraitementAnormal("Erreur : authentification incorrecte.");
+		$msg = "Erreur : authentification incorrecte.";
 	else 
 	{	// mise à jour de la table mrbs_entry_digicode (si besoin) pour créer les digicodes manquants
 		$dao->creerLesDigicodesManquants();
@@ -65,80 +54,84 @@ else
 		$nbReponses = sizeof($lesReservations);
 	
 		if ($nbReponses == 0)
-			TraitementAnormal ("Erreur : vous n'avez aucune réservation.");
+			$msg = "Erreur : vous n'avez aucune réservation.";
 		else
-			TraitementNormal();
+			$msg = "Vous avez effectué " . $nbReponses . " réservation(s).";
 	}
 	// ferme la connexion à MySQL
 	unset($dao);
 }
-// Mise en forme finale   
-$doc->formatOutput = true;  
-// renvoie le contenu XML
-echo $doc->saveXML();
-// fin du programme
+// création du flux XML en sortie
+creerFluxXML ($msg, $lesReservations);
+
+// fin du programme (pour ne pas enchainer sur la fonction qui suit)
 exit;
-
-
-// fonction de traitement des cas anormaux
-function TraitementAnormal($msg)
-{	// redéclaration des données globales utilisées dans la fonction
-	global $doc;
-	// crée l'élément 'data' à la racine du document XML
-	$elt_data = $doc->createElement('data');
-	$doc->appendChild($elt_data);
-	// place l'élément 'reponse' juste après l'élément 'data'
-	$elt_reponse = $doc->createElement('reponse', $msg);
-	$elt_data->appendChild($elt_reponse);
-	return;
-}
  
 
-// fonction de traitement des cas normaux
-function TraitementNormal()
-{	// redéclaration des données globales utilisées dans la fonction
-	global $doc;
-	global $nom, $lesReservations, $nbReponses;
 
+// création du flux XML en sortie
+function creerFluxXML($msg, $lesReservations)
+{	// crée une instance de DOMdocument (DOM : Document Object Model)
+	$doc = new DOMDocument();
+	
+	// specifie la version et le type d'encodage
+	$doc->version = '1.0';
+	$doc->encoding = 'ISO-8859-1';
+	
+	// crée un commentaire et l'encode en ISO
+	$elt_commentaire = $doc->createComment('Service web ConsulterReservations - BTS SIO - Lycée De La Salle - Rennes');
+	// place ce commentaire à la racine du document XML
+	$doc->appendChild($elt_commentaire);
+	
 	// crée l'élément 'data' à la racine du document XML
 	$elt_data = $doc->createElement('data');
 	$doc->appendChild($elt_data);
-	// place l'élément 'reponse' juste après l'élément 'data'
-	$elt_reponse = $doc->createElement('reponse', "Vous avez effectué " . $nbReponses . " réservation(s).");
+	
+	// place l'élément 'reponse' dans l'élément 'data'
+	$elt_reponse = $doc->createElement('reponse', $msg);
 	$elt_data->appendChild($elt_reponse);
 	
+	// place l'élément 'donnees' dans l'élément 'data'
 	$elt_donnees = $doc->createElement('donnees');
-	$elt_data->appendChild($elt_donnees);	
+	$elt_data->appendChild($elt_donnees);
 	
 	// traitement des réservations
-	foreach ($lesReservations as $uneReservation)
-	{
-		// crée un élément vide 'reservation'
-		$elt_reservation = $doc->createElement('reservation');
-		// place l'élément 'reservation' dans l'élément 'donnees'
-		$elt_donnees->appendChild($elt_reservation);
-
-		// crée les éléments enfants de l'élément 'reservation'
-		$elt_id         = $doc->createElement('id', $uneReservation->getId());
-		$elt_reservation->appendChild($elt_id);
-		$elt_timestamp  = $doc->createElement('timestamp', $uneReservation->getTimestamp());
-		$elt_reservation->appendChild($elt_timestamp);
-		$elt_start_time = $doc->createElement('start_time', date('Y-m-d H:i:s', $uneReservation->getStart_time()));
-		$elt_reservation->appendChild($elt_start_time);
-		$elt_end_time   = $doc->createElement('end_time', date('Y-m-d H:i:s', $uneReservation->getEnd_time()));
-		$elt_reservation->appendChild($elt_end_time);
-		$elt_room_name  = $doc->createElement('room_name', $uneReservation->getRoom_name());
-		$elt_reservation->appendChild($elt_room_name);
-		$elt_status     = $doc->createElement('status', $uneReservation->getStatus());
-		$elt_reservation->appendChild($elt_status);
+	if (sizeof($lesReservations) > 0) {
+		foreach ($lesReservations as $uneReservation)
+		{
+			// crée un élément vide 'reservation'
+			$elt_reservation = $doc->createElement('reservation');
+			// place l'élément 'reservation' dans l'élément 'donnees'
+			$elt_donnees->appendChild($elt_reservation);
 		
-		// le digicode n'est renseigné que pour les réservations confirmées
-		if ( $uneReservation->getStatus() == "0")		// réservation confirmée
-			$elt_digicode = $doc->createElement('digicode', utf8_encode($uneReservation->getDigicode()));
-		else										// réservation provisoire
-			$elt_digicode = $doc->createElement('digicode', "");
-		$elt_reservation->appendChild($elt_digicode);
+			// crée les éléments enfants de l'élément 'reservation'
+			$elt_id         = $doc->createElement('id', $uneReservation->getId());
+			$elt_reservation->appendChild($elt_id);
+			$elt_timestamp  = $doc->createElement('timestamp', $uneReservation->getTimestamp());
+			$elt_reservation->appendChild($elt_timestamp);
+			$elt_start_time = $doc->createElement('start_time', date('Y-m-d H:i:s', $uneReservation->getStart_time()));
+			$elt_reservation->appendChild($elt_start_time);
+			$elt_end_time   = $doc->createElement('end_time', date('Y-m-d H:i:s', $uneReservation->getEnd_time()));
+			$elt_reservation->appendChild($elt_end_time);
+			$elt_room_name  = $doc->createElement('room_name', $uneReservation->getRoom_name());
+			$elt_reservation->appendChild($elt_room_name);
+			$elt_status     = $doc->createElement('status', $uneReservation->getStatus());
+			$elt_reservation->appendChild($elt_status);
+		
+			// le digicode n'est renseigné que pour les réservations confirmées
+			if ( $uneReservation->getStatus() == "0")		// réservation confirmée
+				$elt_digicode = $doc->createElement('digicode', utf8_encode($uneReservation->getDigicode()));
+			else										// réservation provisoire
+				$elt_digicode = $doc->createElement('digicode', "");
+			$elt_reservation->appendChild($elt_digicode);
+		}
 	}
+	
+	// Mise en forme finale
+	$doc->formatOutput = true;
+	
+	// renvoie le contenu XML
+	echo $doc->saveXML();
 	return;
 }
 ?>
--- services/ConsulterSalles.php
+++ services/ConsulterSalles.php
@@ -1,6 +1,7 @@
 <?php
 // Service web du projet Réservations M2L
 // Ecrit le 31/3/2016 par Jim
+// Modifié le 3/6/2016 par Jim
 
 // Ce service web permet à un utilisateur de consulter la liste des salles proposées à la réservation
 // et fournit un flux XML contenant un compte-rendu d'exécution
@@ -11,41 +12,30 @@
 // Les paramètres peuvent être passés par la méthode POST (à privilégier en exploitation pour la confidentialité des données) :
 //     http://<hébergeur>/ConsulterSalles.php
 
-// déclaration des variables globales pour pouvoir les utiliser aussi dans les fonctions
-global $doc;		// le document XML à générer
-global $lesSalles, $nbReponses;
-
 // inclusion de la classe Outils
 include_once ('../modele/Outils.class.php');
 // inclusion des paramètres de l'application
 include_once ('../modele/parametres.localhost.php');
-
-// crée une instance de DOMdocument 
-$doc = new DOMDocument();
-
-// specifie la version et le type d'encodage
-$doc->version = '1.0';
-$doc->encoding = 'ISO-8859-1';
-  
-// crée un commentaire et l'encode en ISO
-$elt_commentaire = $doc->createComment('Service web ConsulterSalles - BTS SIO - Lycée De La Salle - Rennes');
-// place ce commentaire à la racine du document XML
-$doc->appendChild($elt_commentaire);
 	
 // Récupération des données transmises
 // la fonction $_GET récupère une donnée passée en paramètre dans l'URL par la méthode GET
 if ( empty ($_GET ["nom"]) == true)  $nom = "";  else   $nom = $_GET ["nom"];
 if ( empty ($_GET ["mdp"]) == true)  $mdp = "";  else   $mdp = $_GET ["mdp"];
+
 // si l'URL ne contient pas les données, on regarde si elles ont été envoyées par la méthode POST
 // la fonction $_POST récupère une donnée envoyées par la méthode POST
 if ( $nom == "" && $mdp == "" )
 {	if ( empty ($_POST ["nom"]) == true)  $nom = "";  else   $nom = $_POST ["nom"];
 	if ( empty ($_POST ["mdp"]) == true)  $mdp = "";  else   $mdp = $_POST ["mdp"];
 }
-  
+
+// initialisation du nombre de salles
+$nbReponses = 0;
+$lesSalles = array();
+
 // Contrôle de la présence des paramètres
 if ( $nom == "" || $mdp == "" )
-{	TraitementAnormal ("Erreur : données incomplètes.");
+{	$msg = "Erreur : données incomplètes.";
 }
 else
 {	// connexion du serveur web à la base MySQL ("include_once" peut être remplacé par "require_once")
@@ -53,75 +43,77 @@ else
 	$dao = new DAO();
 	
 	if ( $dao->getNiveauUtilisateur($nom, $mdp) == "inconnu" )
-		TraitementAnormal("Erreur : authentification incorrecte.");
+		$msg = "Erreur : authentification incorrecte.";
 	else 
 	{	// récupération de la liste des salles
-		$lesSalles = $dao->listeSalles($nom);
-		$nbReponses = sizeof($lesSalles);
-		
-		TraitementNormal();
+		$lesSalles = $dao->getLesSalles($nom);
+		$nbReponses = sizeof($lesSalles);		
+		$msg = $nbReponses . " salles disponibles en réservation.";
 	}
 	// ferme la connexion à MySQL
 	unset($dao);
 }
-// Mise en forme finale   
-$doc->formatOutput = true;  
-// renvoie le contenu XML
-echo $doc->saveXML();
-// fin du programme
+// création du flux XML en sortie
+creerFluxXML ($msg, $lesSalles);
+
+// fin du programme (pour ne pas enchainer sur la fonction qui suit)
 exit;
 
 
-// fonction de traitement des cas anormaux
-function TraitementAnormal($msg)
-{	// redéclaration des données globales utilisées dans la fonction
-	global $doc;
+
+// création du flux XML en sortie
+function creerFluxXML($msg, $lesSalles)
+{	// crée une instance de DOMdocument (DOM : Document Object Model)
+	$doc = new DOMDocument();
+	
+	// specifie la version et le type d'encodage
+	$doc->version = '1.0';
+	$doc->encoding = 'ISO-8859-1';
+	
+	// crée un commentaire et l'encode en ISO
+	$elt_commentaire = $doc->createComment('Service web ConsulterSalles - BTS SIO - Lycée De La Salle - Rennes');
+	// place ce commentaire à la racine du document XML
+	$doc->appendChild($elt_commentaire);
+	
 	// crée l'élément 'data' à la racine du document XML
 	$elt_data = $doc->createElement('data');
 	$doc->appendChild($elt_data);
-	// place l'élément 'reponse' juste après l'élément 'data'
+	
+	// place l'élément 'reponse' dans l'élément 'data'
 	$elt_reponse = $doc->createElement('reponse', $msg);
 	$elt_data->appendChild($elt_reponse);
-	return;
-}
- 
-
-// fonction de traitement des cas normaux
-function TraitementNormal()
-{	// redéclaration des données globales utilisées dans la fonction
-	global $doc;
-	global $lesSalles, $nbReponses;
-		
-	// crée l'élément 'data' à la racine du document XML
-	$elt_data = $doc->createElement('data');
-	$doc->appendChild($elt_data);
-	// place l'élément 'reponse' juste après l'élément 'data'
-	$elt_reponse = $doc->createElement('reponse', $nbReponses . " salles disponibles en réservation.");
-	$elt_data->appendChild($elt_reponse);
 	
+	// place l'élément 'donnees' dans l'élément 'data'
 	$elt_donnees = $doc->createElement('donnees');
 	$elt_data->appendChild($elt_donnees);
 	
 	// traitement des salles
-	foreach ($lesSalles as $uneSalle)
-	{
-		// crée un élément vide 'salle'
-		$elt_salle = $doc->createElement('salle');
-		// place l'élément 'salle' dans l'élément 'donnees'
-		$elt_donnees->appendChild($elt_salle);
-
-		// crée les éléments enfants de l'élément 'reservation'
-		$elt_id         = $doc->createElement('id', utf8_encode($uneSalle->getId()));
-		$elt_salle->appendChild($elt_id);
-		$elt_room_name  = $doc->createElement('room_name', $uneSalle->getRoom_name());
-		$elt_salle->appendChild($elt_room_name);
-		$elt_capacity  = $doc->createElement('capacity', $uneSalle->getCapacity());
-		$elt_salle->appendChild($elt_capacity);
-		$elt_area_name = $doc->createElement('area_name', $uneSalle->getAeraName());
-		$elt_salle->appendChild($elt_area_name);
-		$elt_area_admin_email   = $doc->createElement('area_admin_email', $uneSalle->getAeraAdminEmail());
-		$elt_salle->appendChild($elt_area_admin_email);
+	if (sizeof($lesSalles) > 0) {
+		foreach ($lesSalles as $uneSalle)
+		{
+			// crée un élément vide 'salle'
+			$elt_salle = $doc->createElement('salle');
+			// place l'élément 'salle' dans l'élément 'donnees'
+			$elt_donnees->appendChild($elt_salle);
+		
+			// crée les éléments enfants de l'élément 'reservation'
+			$elt_id        = $doc->createElement('id', utf8_encode($uneSalle->getId()));
+			$elt_salle->appendChild($elt_id);
+			$elt_room_name = $doc->createElement('room_name', $uneSalle->getRoom_name());
+			$elt_salle->appendChild($elt_room_name);
+			$elt_capacity  = $doc->createElement('capacity', $uneSalle->getCapacity());
+			$elt_salle->appendChild($elt_capacity);
+			$elt_area_name = $doc->createElement('area_name', $uneSalle->getAreaName());
+			$elt_salle->appendChild($elt_area_name);
+			$elt_area_admin_email   = $doc->createElement('area_admin_email', $uneSalle->getAreaAdminEmail());
+			$elt_salle->appendChild($elt_area_admin_email);
+		}
 	}
+	// Mise en forme finale
+	$doc->formatOutput = true;
+	
+	// renvoie le contenu XML
+	echo $doc->saveXML();
 	return;
 }
 ?>
